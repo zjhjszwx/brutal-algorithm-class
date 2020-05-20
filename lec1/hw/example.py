b416b9bc4a4da7929512c6cbd4e9556916fb99b2
CreatCodeBuild <CreatCodeBuild@github.com>
2020-05-19 23:55:23 -0700

    week 1 homework

--- lec1/hw/example.py
+++
@@ -1,8 +0,0 @@
-# Example Homework
-def sum(vector):
-    s = 0
-    for num in vector:
-        s += num
-    return s
-
-print(sum([1, 2, 3]))
--- lec1/hw/hw_example.py
+++
@@ -1,7 +0,0 @@
-def sum(vector):
-    s = 0
-    for num in vector:
-        s += num
-    return s
-
-print(sum([1, 2, 3]))
