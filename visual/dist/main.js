d0b394b6be7d02b3076953a8144f5ca69a9bf56e
哲の王 <albertcodelab@gmail.com>
2020-05-28 23:20:31 +0000

    infinite animation

--- visual/dist/main.js
+++ visual/dist/main.js
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import { chan, select } from 'https://creatcodebuild.github.io/csp/dist/csp.js';
+import { MergeSort, InsertionSort, infinite } from './sort.js';
 function SortVisualizationComponent(id, arrays) {
     var _a;
     let ele = document.getElementById(id);
@@ -63,75 +64,6 @@ function sleep(time) {
         setTimeout(resolve, time);
     });
 }
-async function InsertionSort(array, reactor) {
-    function insert(array, number) {
-        // [1, 2, 4, 5], 3
-        // in-place
-        // immutable 不可变
-        if (array.length === 0) {
-            return [number];
-        }
-        let sorted = [];
-        let inserted = false;
-        for (let i = 0; i < array.length; i++) { // n
-            if (!inserted) {
-                if (number < array[i]) {
-                    inserted = true;
-                    sorted.push(number);
-                }
-            }
-            sorted.push(array[i]);
-        }
-        if (!inserted) {
-            sorted.push(number);
-        }
-        return sorted;
-    }
-    let sortedArray = [];
-    for (let i = 0; i < array.length; i++) { // n
-        sortedArray = insert(sortedArray, array[i]);
-        await reactor.put(sortedArray.concat(array.slice(i + 1)));
-    }
-    return sortedArray;
-}
-async function MergeSort(array, reactor) {
-    async function merge(l, r, startIndex) {
-        if (l.length === 0) {
-            return r;
-        }
-        if (r.length === 0) {
-            return l;
-        }
-        let shifted = await (async () => {
-            if (l[0] < r[0]) {
-                return l.slice(0, 1).concat(await merge(l.slice(1), r, startIndex + 1));
-            }
-            else {
-                return r.slice(0, 1).concat(await merge(l, r.slice(1), startIndex + 1));
-            }
-        })();
-        // console.log(shifted, startIndex)
-        await reactor.put([shifted, startIndex]);
-        return shifted;
-    }
-    async function sort(array, startIndex) {
-        if (array.length <= 1) {
-            return array;
-        }
-        let m = Math.floor(array.length / 2);
-        let l = array.slice(0, m);
-        let r = array.slice(m);
-        let sortedL = await sort(l, startIndex);
-        let sortedR = await sort(r, startIndex + m);
-        await reactor.put([sortedL.concat(sortedR), startIndex]);
-        // need global index here to correctly animate
-        let merged = await merge(sortedL, sortedR, startIndex);
-        await reactor.put([merged, startIndex]);
-        return merged;
-    }
-    await reactor.put([array, 0]);
-    return await sort(array, 0);
-}
 async function main() {
     // init an array
     let array = [];
@@ -145,9 +77,11 @@ async function main() {
     let resume = chan();
     // controlButton(stop, resume);
     console.log('begin sort', array);
-    let s1 = InsertionSort(array, insertQueue);
-    let s2 = MergeSort(array, mergeQueue);
-    console.log('after sort');
+    infinite(InsertionSort, array, insertQueue);
+    infinite(MergeSort, array, mergeQueue);
+    // let s1 = InsertionSort(array, insertQueue);
+    // let s2 = MergeSort(array, mergeQueue);
+    // console.log('after sort');
     let mergeQueue2 = (() => {
         let c = chan();
         (async () => {
--- visual/src/main.ts
+++ visual/src/main.ts
@@ -1,5 +1,6 @@
 // @ts-nocheck
 import { chan, Channel, select } from 'https://creatcodebuild.github.io/csp/dist/csp.js';
+import { MergeSort, InsertionSort, infinite } from './sort.js';
 
 function SortVisualizationComponent(id: string, arrays: Channel<number[]>) {
 
@@ -74,80 +75,7 @@ function sleep(time) {
     })
 }
 
-async function InsertionSort(array, reactor: Channel<number[]>) {
 
-    function insert(array, number) {
-        // [1, 2, 4, 5], 3
-        // in-place
-        // immutable 不可变
-        if (array.length === 0) {
-            return [number];
-        }
-        let sorted = [];
-        let inserted = false;
-        for (let i = 0; i < array.length; i++) { // n
-            if (!inserted) {
-                if (number < array[i]) {
-                    inserted = true;
-                    sorted.push(number);
-                }
-            }
-            sorted.push(array[i]);
-        }
-        if (!inserted) {
-            sorted.push(number);
-        }
-        return sorted;
-    }
-
-    let sortedArray = [];
-    for (let i = 0; i < array.length; i++) { // n
-        sortedArray = insert(sortedArray, array[i]);
-        await reactor.put(sortedArray.concat(array.slice(i + 1)));
-    }
-    return sortedArray;
-}
-
-
-async function MergeSort(array, reactor: Channel<[number[], number]>) {
-
-    async function merge(l: number[], r: number[], startIndex: number): Promise<number[]> {
-        if (l.length === 0) {
-            return r
-        }
-        if (r.length === 0) {
-            return l
-        }
-        let shifted: number[] = await (async () => {
-            if (l[0] < r[0]) {
-                return l.slice(0, 1).concat(await merge(l.slice(1), r, startIndex + 1))
-            } else {
-                return r.slice(0, 1).concat(await merge(l, r.slice(1), startIndex + 1))
-            }
-        })();
-        // console.log(shifted, startIndex)
-        await reactor.put([shifted, startIndex]);
-        return shifted;
-    }
-
-    async function sort(array, startIndex): Promise<number[]> {
-        if (array.length <= 1) {
-            return array;
-        }
-        let m = Math.floor(array.length / 2)
-        let l = array.slice(0, m)
-        let r = array.slice(m)
-        let sortedL = await sort(l, startIndex)
-        let sortedR = await sort(r, startIndex + m)
-        await reactor.put([sortedL.concat(sortedR), startIndex]);
-        // need global index here to correctly animate
-        let merged = await merge(sortedL, sortedR, startIndex)
-        await reactor.put([merged, startIndex]);
-        return merged;
-    }
-    await reactor.put([array, 0]);
-    return await sort(array, 0);
-}
 
 
 async function main() {
@@ -166,9 +94,11 @@ async function main() {
 
 
     console.log('begin sort', array);
-    let s1 = InsertionSort(array, insertQueue);
-    let s2 = MergeSort(array, mergeQueue);
-    console.log('after sort');
+    infinite(InsertionSort, array, insertQueue);
+    infinite(MergeSort, array, mergeQueue)
+    // let s1 = InsertionSort(array, insertQueue);
+    // let s2 = MergeSort(array, mergeQueue);
+    // console.log('after sort');
 
 
     let mergeQueue2 = (() => {
